684cc0f7b2b061b144b9eba290ecf81f86b46472
Jared Allard <RainbowDashDC@users.noreply.github.com>
2014-10-28 07:06:53 -0700

    Re-add Windows XP support.

--- targetver.h
+++ targetver.h
@@ -1,9 +1,9 @@
 
 /** Windows Constants **/
-#define WINVER 0x0601           // Windows 7
-#define _WIN32_WINNT 0x0601     // Windows 7
-#define _WIN32_WINDOWS 0x0601   // Windows 7
-#define _WIN32_IE 0x0900        // Internet Explorer 9
+#define WINVER 0x0501           // Windows XP
+#define _WIN32_WINNT 0x0501     // Windows XP
+#define _WIN32_WINDOWS 0x0501   // Windows XP
+#define _WIN32_IE 0x0600        // Internet Explorer 6
 
 // NTDDI_VERSION
-#define NTDDI_VERSION 0x06010000
+#define NTDDI_VERSION 0x05010000
